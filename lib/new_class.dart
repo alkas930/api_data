765f4f75ad0b79c35414eee7ec6b5a6e07728d6e
xpsAlka <alka.xportsoft@gmail.com>
2024-04-26 16:28:19 +0530

    updated2

--- lib/new_class.dart
+++ lib/new_class.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -30,47 +32,224 @@ class NewClass extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('New CLass'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(title),
-            Image.network(
-              ImagePath,
-            ),
-            Text(description),
-            Text(brand),
-            Text(category),
-            Text(discountPercentage),
-            Container(
-              width: 70,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    rating,
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Icon(
-                    Icons.star,
-                    color: Colors.white,
-                  )
-                ],
+        actions: [
+          Row(
+            children: [
+              Icon(
+                Icons.search,
+                color: Colors.blue,
               ),
-              decoration: BoxDecoration(
-                  color: Color.fromARGB(192, 8, 245, 48),
-                  borderRadius: BorderRadius.all(Radius.circular(10))),
-            ),
-            Row(
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Icon(
+                Icons.shopping_cart,
+                color: Colors.purple,
+              )
+            ],
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
               children: [
-                Text("with special offer "),
-                Text(price),
+                Container(
+                  clipBehavior: Clip.antiAlias,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(10)),
+                  child: Image.network(
+                    ImagePath,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text(title,
+                    style: TextStyle(
+                        color: const Color.fromARGB(255, 0, 0, 0),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15)),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Divider(
+                    height: 10,
+                  ),
+                ),
+                Text(
+                  description,
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 22, right: 22, top: 22),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        brand,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              Icon(Icons.favorite_border),
+                              Text("Wishlist")
+                            ],
+                          ),
+                          SizedBox(width: 10),
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.share,
+                                color: Colors.blue,
+                              ),
+                              Text("share")
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Divider(
+                    height: 10,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      width: 70,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            rating,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Icon(
+                            Icons.star,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(192, 220, 103, 7),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      stock,
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    Text(" ratings", style: TextStyle(color: Colors.blue))
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.attach_money_sharp,
+                      color: Colors.green,
+                    ),
+                    Text(
+                      price,
+                      style: TextStyle(color: Colors.green),
+                    ),
+                    Text(
+                      " with 2 Special offers",
+                      style: TextStyle(color: Colors.green),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          child: Icon(Icons.fire_truck),
+                          backgroundColor: Colors.teal,
+                        ),
+                        Text(
+                          "  Free\nDelivery",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          child: Icon(
+                            Icons.find_replace,
+                          ),
+                          backgroundColor: Colors.teal,
+                        ),
+                        Text(
+                          " 10 days\nReplacement",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          child: Icon(Icons.wallet_membership),
+                          backgroundColor: Colors.teal,
+                        ),
+                        Text(
+                          " 36 Month\nwarranty",
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.shopping_cart,
+                          color: Colors.purple,
+                        ),
+                        Text("Add to cart"),
+                      ],
+                    )),
+                SizedBox(height: 30),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.arrow_circle_right_rounded,
+                          color: Colors.purple,
+                        ),
+                        Text("Buy Now"),
+                      ],
+                    ))
               ],
             ),
-            Text(stock),
-          ],
+          ),
         ),
       ),
     );
--- lib/page.dart
+++ lib/page.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:animated_rating_stars/animated_rating_stars.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:project1/icons.dart';
 import 'package:project1/new_class.dart';
 import 'package:project1/posts_model.dart';
 
@@ -15,7 +17,7 @@ class ProductPage extends StatefulWidget {
 class _ProductPageState extends State<ProductPage> {
   List product = [];
 
-  Future<void> fetchProduct() async {
+  fetchProduct() async {
     try {
       final response =
           await http.get(Uri.parse("https://dummyjson.com/product"));
@@ -43,18 +45,65 @@ class _ProductPageState extends State<ProductPage> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Loading...'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 42, 44, 158),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'Limited Time Deals',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.punch_clock,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                    SizedBox(width: 5),
+                    Text(
+                      "offer ends soon",
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              child: Icon(Icons.arrow_right),
+            )
+          ],
         ),
-        body: ListView.builder(
+      ),
+      body: Expanded(
+        child: GridView.builder(
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           itemCount: product.length,
           itemBuilder: (context, index) {
-            return Container(
-              child: Column(
-                children: [
-                  Text(product[index].title),
-                  InkWell(
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                clipBehavior: Clip.antiAlias,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color.fromARGB(255, 245, 248, 249),
+                    border: Border.all(color: Colors.grey)),
+                height: size.height,
+                width: size.width,
+                child: Column(
+                  children: [
+                    InkWell(
                       onTap: () {
                         Navigator.push(
                           context,
@@ -76,13 +125,74 @@ class _ProductPageState extends State<ProductPage> {
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
-                        child:
-                            Image.network("https://picsum.photos/id/1/200/300"),
-                      ))
-                ],
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            "https://picsum.photos/id/1/200/300",
+                            fit: BoxFit.contain,
+                            scale: 2.3,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            product[index].title,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            "Buds T300 with .....",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 18, 18, 18),
+                                fontSize: 13),
+                          ),
+                        ),
+                      ],
+                    ),
+                    AnimatedRatingStars(
+                        onChanged: (v) {},
+                        customFilledIcon: Icons.star,
+                        customHalfFilledIcon: Icons.star_half,
+                        customEmptyIcon: Icons.star_border),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Row(
+                        children: [
+                          Text(
+                            "Price  ",
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 13, 225, 24)),
+                          ),
+                          Text(
+                            product[index].price.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             );
           },
-        ));
+        ),
+      ),
+    );
   }
 }
